8e276ddd83d79507ba013aa2fc65d9785da8cc83
RupertG56 <RupertG56@3caba792-b10b-11de-9ad3-fd5786690477>
2009-11-03 03:47:30 +0000

    Added database constants

--- src/library/const/constants.php
+++ src/library/const/constants.php
@@ -2,8 +2,9 @@
 /**
  * Database defines
  */
-define("DB_HOST", "");
-define("DB_PWD", "");
-define("DB_DATABASE", "");
+define("DB_HOST", "localhost");
+define("DB_UNAME", "devnull");
+define("DB_PWD", "Recycleb!n");
+define("DB_DATABASE", "3750-09devnull");
 
 ?>
