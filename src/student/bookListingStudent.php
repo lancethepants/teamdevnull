2a20eafb1a62c3c244825f8a831e72b0375c66dd
dpham110 <dpham110@3caba792-b10b-11de-9ad3-fd5786690477>
2009-11-10 00:28:11 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/src/student/bookListingStudent.php

---
+++ src/student/bookListingStudent.php
@@ -0,0 +1,77 @@
+<html>
+<head>
+	<link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+<center>
+<div class="content">
+	<div class="header">
+		<img src="title.png" class="title_bar" />
+		<div class="login_box">
+			<input type="text" class="login_input" /><input type="password" class="login_input" />		
+		</div>
+		
+		<div class="header_content">		
+			<div class="menu" align="center" style="font-size:12px;"><br /><Br />
+          
+            </div>
+			<div class="post_it"></div>            
+		</div>
+		
+		<div class="body_content">
+	        <center>
+            <h2 style="text-decoration:underline;">Textbook Listing</h2>
+			<table border="1" cellspacing="0" cellpadding="3">
+            <tr>
+            	<th>Semester</th>
+            	<th>Course</th>
+            	<th>Title</th>
+                <th>Author</th>
+                <th>ISBN</th>
+                <th>Notes</th>
+            </tr>
+            <tr style="font-size:12px;">
+            	<td>Fall 2009</td>
+            	<td>CS 3750</td>
+                <td>Software Estimation</td>
+                <td>Steve McConnell</td>
+                <td>978-0735605350</td>
+                <td>Buy now, but will not be required until CS 4750</td>
+            </tr>
+            <tr style="font-size:12px;">
+            	<td>Fall 2009</td>
+            	<td>CS 4110</td>
+                <td>Intro to Computer Theory</td>
+                <td>Daniel Cohen</td>
+                <td>978-0471137726</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr style="font-size:12px;">
+            	<td>Spring 2010</td>
+            	<td>CS 3030</td>
+                <td>Scripting for Beginners</td>
+                <td>Joe Gorley</td>
+                <td>978-2874755754</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr style="font-size:12px;">
+            	<td>Spring 2010</td>
+            	<td>CS 3030</td>
+                <td>Professional Scripting</td>
+                <td>Gordon James</td>
+                <td>978-1928374747</td>
+                <td>&nbsp;</td>
+            </tr>
+            </table>
+            </center>
+		</div>
+			
+	</div>
+
+</div>
+
+</center>
+
+</body>
+</html>
