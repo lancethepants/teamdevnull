fb599c58690dd02e5bee71015119072bda83f8d2
RupertG56 <RupertG56@3caba792-b10b-11de-9ad3-fd5786690477>
2009-11-10 01:47:35 +0000

    Require this to access the DAL object and possible others

--- trunk/src/includes.php
+++ trunk/src/includes.php
@@ -1,5 +1,12 @@
 <?php
-	require_once("library/const/constants.php");
+	/**
+	 * The root path constant
+	 */
+	define("ROOT", getcwd());
+	define("CLASSES", "/library/classes");
+	define("CONST", "/library/const");
+	
+	require_once(ROOT . CONST . "/constants.php");
 	require_once(ROOT . CLASSES . "/DALAbstract.php");
 	require_once(ROOT . CLASSES . "/DAL.php");
 ?>
